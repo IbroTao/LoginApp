a1d410d0dae664e2be08440955d4a4d4e062daed
Taofeek <ibrahimtaofeek005@gmail.com>
2024-05-03 10:12:00 +0100

    New: react demo up, working and running

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,7 +1,18 @@
+import autoprefixer from "autoprefixer";
 import React from "react";
 
 export default function Username() {
     return (
-        <div>Username</div>
+        <div className="container mx-auto">
+            <div className="flex justify-center items-center h-screen">
+                <div>
+
+                    <div className="title">
+                        <h4>Hello Again!</h4>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     )
 }
