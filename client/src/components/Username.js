fcf39f011a9405cec47e3b7c50b402fee113c108
Taofeek <ibrahimtaofeek005@gmail.com>
2024-05-03 11:00:04 +0100

    New: home page created and styled

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import avatar from '../assets/profile.png'
+
+import styles from '../styles/Username.module.css'
 
 export default function Username() {
     return (
         <div className="container mx-auto">
-            <div className="flex justify-center items-center h-screen">
-                <div>
+            <div className="flex h-screen">
+                <div className={styles.glass}>
 
                     <div className="title flex flex-col items-center">
                         <h4 className="text-5xl font-bold">Hello Again!</h4>
@@ -13,6 +17,21 @@ export default function Username() {
                         </span>
                     </div>
 
+                    <form className="py-1">
+                        <div className="profile flex justify-center py-4">
+                            <img src={avatar} alt="avatar"></img>
+                        </div>
+
+                        <div className="textbox flex flex-col items-center gap-6 ">
+                            <input type="text" placeholder="Username"/>
+                            <button type="submit">Let's Go</button>
+                        </div>
+
+                        <div className="text-center py-4">
+                            <span className="text-gray-500 ">Not a Member <Link className='text-red-500' to ="/register">Register Now</Link></span>
+                        </div>
+
+                    </form>
                 </div>
             </div>
         </div>
