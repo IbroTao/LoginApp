09e25b70e6627fe787e3a2ed2e45c35e553a3c40
Taofeek <ibrahimtaofeek005@gmail.com>
2024-05-03 10:02:14 +0100

    New: react routes added in client folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,44 @@
 import React from 'react'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
+/**import all components*/
+import Password from './components/Password'
+import Username from './components/Username'
+import Register from './components/Register'
+import Profile from './components/Profile'
+import Recovery from './components/Recovery'
+import Reset from './components/Reset'
+import PageNotFound from './components/PageNotFound'
+
 /** root routes */
 const router =  createBrowserRouter([
     {
         path: '/',
-        element: <div>Root Routes</div>
+        element: <Username></Username>
     },
     {
         path: '/register',
-        element: <div>Register Routes</div>
+        element: <Register></Register>
+    },
+    {
+        path: '/password',
+        element: <Password></Password>
+    },
+    {
+        path: '/profile',
+        element: <Profile></Profile>
+    },
+    {
+        path: '/recovery',
+        element: <Recovery></Recovery>
+    },
+    {
+        path: '/reset',
+        element: <Reset></Reset>
+    },
+    {
+        path: '*',
+        element: <PageNotFound></PageNotFound>
     },
 ])
 
