35679d9b31ad4d13e333ab154b418316464ee7bc
Taofeek <ibrahimtaofeek005@gmail.com>
2024-05-04 15:27:04 +0100

    New: controllers for routes set up

---
+++ server/controllers/appController.js
@@ -0,0 +1,25 @@
+
+
+export async function register(req, res) {
+    res.json("register route")
+}
+
+export async function login(req, res) {
+    res.json("login route")
+}
+
+export async function getUser(req, res) {
+    res.json("getUser route")
+};
+
+export async function updateUser(req, res) {
+    res.json("updateUser route")
+}
+
+export async function generateOTP(req, res) {
+    res.json("generateOTP route")
+}
+
+export async function verifyOTP(req, res) {
+    res.json("verifyOTP route")
+}
