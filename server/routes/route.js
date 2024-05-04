f03798d6d11228ddaf2e492f884634c83b089ae4
Taofeek <ibrahimtaofeek005@gmail.com>
2024-05-04 15:14:26 +0100

    New: write comment details for user routes

--- server/routes/route.js
+++ server/routes/route.js
@@ -3,20 +3,20 @@ const router = Router();
 
 
 /** POST Methods */
-router.route('/register').post(async(req, res) => res.json('register route'))
-router.route('/registerMail').post();
-router.route('/authenticate').post();
-router.route('/login').post();
+router.route('/register').post(async(req, res) => res.json('register route')); // register user
+router.route('/registerMail').post(); // send the email
+router.route('/authenticate').post(); // authenticate user
+router.route('/login').post(); // login in app
 
 /** GET Methods */
-router.route('/user/:username').get();
-router.route('/generateOTP').get();
-router.route('/verifyOTP').get();
-router.route('/createResetSession').get();
+router.route('/user/:username').get(); // user with username
+router.route('/generateOTP').get(); // generate random OTP
+router.route('/verifyOTP').get(); // verify generated OTP
+router.route('/createResetSession').get(); // reset all the variables
 
 /** PUT Methods */
-router.route('/updateUser').put();
-router.route('/resetPassword').put();
+router.route('/updateUser').put(); // update user profile 
+router.route('/resetPassword').put(); // reset user password
 
 
 export default router;
