809b2f5b65ddb4f6b7e953347d783585cdd54fda
Taofeek <ibrahimtaofeek005@gmail.com>
2024-05-04 04:25:35 +0100

    Fix: #002 - register UI bug fixed

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -36,22 +36,22 @@ export default function Register() {
     }
 
     return (
-        <div className="container mx-auto mb-8">
+        <div className="container mx-auto">
 
             <Toaster position="top-center" reverseOrder={false}></Toaster>
 
-            <div className="flex justify-center items-center h-screen">
-                <div className={styles.glass} style={{width: '45%'}}>
+            <div className="flex justify-center items-center h-screen mb-8">
+                <div className={styles.glass} style={{width: '40%'}}>
 
                     <div className="title flex flex-col items-center">
                         <h4 className="text-5xl font-bold">Register</h4>
-                        <span className="py-4 text-xl w-2/3 text-center text-gray-500">
+                        <span className="py-3 text-xl w-2/3 text-center text-gray-500">
                             Happy to join you!
                         </span>
                     </div>
 
                     <form className="py-1" onSubmit={formik.handleSubmit}>
-                        <div className="profile flex justify-center py-4">
+                        <div className="profile flex justify-center py-3">
                             <label htmlFor="profile">
                                 <img src={file || avatar } className={styles.profile_img} alt="avatar"></img>
                             </label>
@@ -59,20 +59,27 @@ export default function Register() {
                             <input onChange={onUpload} type="file" id="profile" name="profile"/>
                         </div>
 
-                        <div className="textbox flex flex-col items-center gap-6 ">
+                        <div className="textbox flex flex-col items-center gap-6">
                             <input {...formik.getFieldProps('email')} className={styles.textbox} type="text" placeholder="Email*"/>
                             <input {...formik.getFieldProps('username')} className={styles.textbox} type="text" placeholder="Username*"/>
                             <input {...formik.getFieldProps('password')} className={styles.textbox} type="text" placeholder="Password"/>
                             <button className={styles.btn} type="submit">Register</button>
                         </div>
 
-                        <div className="text-center py-4">
-                            <span className="text-gray-500 ">Already Register? <Link className='text-red-500' to ="/">Login Now</Link></span>
+                        <div className="wrapper">
+                            <div className="text-center py-3">
+                                <span className="text-gray-500 ">Already Register? <Link className='text-red-500' to="/">Login Now</Link></span>
+                            </div>
                         </div>
-
                     </form>
                 </div>
             </div>
+
+            <style jsx>{`
+            .wrapper {
+                margin-bottom: 20px; // Adjust the value as needed
+            }
+        `}</style>
         </div>
     )
 }
