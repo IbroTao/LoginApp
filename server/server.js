039afe50ec8b9ab6fca253e3a9ffc7295ca12eea
Taofeek <ibrahimtaofeek005@gmail.com>
2024-05-04 13:15:17 +0100

    New: index file set up for server

--- server/server.js
+++ server/server.js
@@ -5,9 +5,20 @@ import morgan from "morgan";
 
 const app = express();
 
-/**middlewares */
+/** middlewares */
 app.use(express.json());
 app.use(cors());
 app.use(morgan('tiny'));
 app.disable('x-powered-by'); // when disabled hackers do not know the stack used
 
+const port = 8080;
+
+/** HTTP GET Request */
+app.get('/', (req, res) => {
+    res.status(200).json("HOME GET REQUEST")
+})
+
+/** start server */
+app.listen(port, () => {
+    console.log(`Server connected to http://localhost:${port}`);
+})
