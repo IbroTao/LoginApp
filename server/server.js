cb61ebfaeb5031b1b81ae564038f63750a3d2e7a
Taofeek <ibrahimtaofeek005@gmail.com>
2024-05-04 13:03:56 +0100

    New: index file set up for server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+
+const app = express();
+
+/**middlewares */
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
+app.disable('x-powered-by'); // when disabled hackers do not know the stack used
+
