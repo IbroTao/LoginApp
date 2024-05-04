30705431564c22bf192eabcc25e3beda78db41dc
Taofeek <ibrahimtaofeek005@gmail.com>
2024-05-04 15:03:34 +0100

    New: different endpoints set up for user auth

--- server/routes/route.js
+++ server/routes/route.js
@@ -4,6 +4,9 @@ const router = Router();
 
 /** POST Methods */
 router.route('/register').post(async(req, res) => res.json('register route'))
+router.route('/registerMail').post();
+router.route('/authenticate').post();
+router.route('/login').post();
 
 /** GET Methods */
 
