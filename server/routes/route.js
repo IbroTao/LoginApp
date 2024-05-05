4e18cd9c4d17eceffe4d7f9fe4619b63f7ae94e4
Taofeek <ibrahimtaofeek005@gmail.com>
2024-05-05 16:33:44 +0100

    New: local variables configured for otp generation

--- server/routes/route.js
+++ server/routes/route.js
@@ -3,7 +3,7 @@ const router = Router();
 
 /** import all controllers */
 import * as controller from '../controllers/appController.js'
-import Auth from "../middleware/auth.js"
+import Auth, {localVariables} from "../middleware/auth.js"
 
 
 /** POST Methods */
@@ -14,7 +14,7 @@ router.route('/login').post(controller.verifyUser, controller.login); // login i
 
 /** GET Methods */
 router.route('/user/:username').get(controller.getUser); // user with username
-router.route('/generateOTP').get(controller.generateOTP); // generate random OTP
+router.route('/generateOTP').get(controller.verifyUser, localVariables, controller.generateOTP); // generate random OTP
 router.route('/verifyOTP').get(controller.verifyOTP); // verify generated OTP
 router.route('/createResetSession').get(controller.createResetSession); // reset all the variables
 
