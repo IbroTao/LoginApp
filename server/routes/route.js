f1bb7473b7f9ec71b6c4060764d09ca9946f1481
Taofeek <ibrahimtaofeek005@gmail.com>
2024-05-08 21:57:37 +0100

    new: email sender integrated into router

--- server/routes/route.js
+++ server/routes/route.js
@@ -3,12 +3,13 @@ const router = Router();
 
 /** import all controllers */
 import * as controller from '../controllers/appController.js'
+import {registerMail} from "../controllers/mailer.js"
 import Auth, {localVariables} from "../middleware/auth.js"
 
 
 /** POST Methods */
 router.route('/register').post(controller.register); // register user
-//router.route('/registerMail').post(); // send the email
+router.route('/registerMail').post(registerMail); // send the email
 router.route('/authenticate').post((req, res) => res.end()); // authenticate user
 router.route('/login').post(controller.verifyUser, controller.login); // login in app
 
