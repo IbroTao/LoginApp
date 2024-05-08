756adf97ed763da5dc512858f230ccdf3799903e
Taofeek <ibrahimtaofeek005@gmail.com>
2024-05-08 13:47:02 +0100

    New: email sending feature added

--- server/controllers/mailer.js
+++ server/controllers/mailer.js
@@ -2,3 +2,14 @@ import nodemailer from "nodemailer";
 import MailGen from "mailgen"
 
 import dotenv from "dotenv"
+dotenv.config();
+
+let nodeConfig = {
+    host: "smtp.etheral.email",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.ETHERAL_USERNAME,
+        pass: process.env.ETHERAL_PASSWORD
+    }
+}
