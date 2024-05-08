5d8953367f50b2700cf5816f126f85683e23b186
Taofeek <ibrahimtaofeek005@gmail.com>
2024-05-08 21:54:29 +0100

    feature: more conditions to be met before email sending

--- server/controllers/mailer.js
+++ server/controllers/mailer.js
@@ -30,7 +30,25 @@ export const registerMail = async (req, res) => {
     // body of the email
     let email = {
         body: {
-            
+            name: username,
+            intro: text || "Welcome to Daily Tuition! We\'re exicted to have you on board.",
+            outro: 'Need help, or have questions? Just reply to this email, we\'d love to help.'
         }
     }
+
+    let emailBody = MailGenerator.generate(email);
+
+    let message = {
+        from: process.env.ETHERAL_USERNAME,
+        to: userEmail,
+        subject: subject || "Signup Successful",
+        html: emailBody
+    }
+
+    // send mail
+    transporter.sendMail(message)
+        .then(() => {
+            return res.status(200).send({ msg: "You should receive an email from us. "})
+        })
+        .catch(error => res.status(500).send({error}))
 }
