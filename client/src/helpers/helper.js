f40a274754e10306933961d49ffa221f3ca0c862
Taofeek <ibrahimtaofeek005@gmail.com>
2024-05-09 21:39:35 +0100

    new: axios set up for user auth

--- client/src/helpers/helper.js
+++ client/src/helpers/helper.js
@@ -1,2 +1,13 @@
+import axios from "axios"
 
 /** MAKE API REQUEST */
+
+
+/** authenticate function */
+export async function authenticate(username) {
+    try {
+        return await axios.post('/api/authenticate')
+    } catch (error) {
+        return ({error: "Username doesn't exist"})
+    }
+}
