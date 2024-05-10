6e00e70b7415c92d18f86e8fbb813aa62bb624a0
Taofeek <ibrahimtaofeek005@gmail.com>
2024-05-10 15:20:57 +0100

    new: get user details using axios

--- client/src/helpers/helper.js
+++ client/src/helpers/helper.js
@@ -6,8 +6,17 @@ import axios from "axios"
 /** authenticate function */
 export async function authenticate(username) {
     try {
-        return await axios.post('/api/authenticate')
+        return await axios.post('/api/authenticate', {username})
     } catch (error) {
         return ({error: "Username doesn't exist"})
+    }
+}
+
+/** get user details */
+export async function getUser({username}){
+    try {
+        
+    } catch (error) {
+        return {error: "Password doesn't match"}
     }
 }
