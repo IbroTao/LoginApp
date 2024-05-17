99cd0a0c8a262262cc2193c926d9bb1af16b8987
Taofeek <ibrahimtaofeek005@gmail.com>
2024-05-17 20:04:10 +0100

    new: otp verification using axios

--- client/src/helpers/helper.js
+++ client/src/helpers/helper.js
@@ -85,7 +85,8 @@ export async function generateOTP(username){
 /** verify OTP */
 export async function verifyOTP({username, code}){
     try {
-        await axios.get('/api/verifyOTP', {params: {username, code}})
+        const {data, status} = await axios.get('/api/verifyOTP', {params: {username, code}});
+        return {data, status};
     } catch (error) {
         return Promise.reject(error);
     }
